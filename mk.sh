a9738d5c90f288a0ca5cd0dd6556acbf3b242df7
jianxinpanpan <jianxin_pan@163.com>
2018-04-30 22:15:06 -0700

    qemu src, build for ubuntu 16.04

---
+++ mk.sh
@@ -0,0 +1,5 @@
+
+./configure --target-list=arm-softmmu,i386-softmmu,x86_64-softmmu,aarch64-linux-user,arm-linux-user,i386-linux-user,x86_64-linux-user,aarch64-softmmu --audio-drv-list= --enable-virtfs
+
+make -j4
+#make install
